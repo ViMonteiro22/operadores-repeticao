dd6ff6676f4be000f1ce87d3b65e1c851ea4a86e
victoriamonteiro <victoria.vrsm@gmail.com>
2026-09-03 12:43:16 -0300

    Testando o FOR

--- src/EstruturasDeRepeticao01.java
+++ src/EstruturasDeRepeticao01.java
@@ -7,7 +7,7 @@ public class EstruturasDeRepeticao01 {
             count += 1;
         } */
 
-        int pratosSujos = 3;
+        /* int pratosSujos = 3;
 
         while (pratosSujos > 0) {
             System.out.println("Lavando um prato...");
@@ -17,6 +17,22 @@ public class EstruturasDeRepeticao01 {
 
         do {
             System.out.println("Lavando um prato (do-while)");
-        } while (pratosSujos > 0);
+        } while (pratosSujos > 0); */
+
+        for (int a = 1; a <= 10; a++){
+            for (int b = 0; b <= 10; b++){
+                if (b == 0){
+                    System.out.println("Tabuada do " + a);
+                }
+                System.out.println(a + "x" + b + " = " + a*b);
+                if (b == 10){
+                    System.out.println("------------------------");
+                }
+            }
+        }
+
+       /* for (int i = 1; i <= 5; i++) {
+            System.out.println("Escrevendo a tabuada pela " + i + "ª vez.");
+        } */
     }
 }
