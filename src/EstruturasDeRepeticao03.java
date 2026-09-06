4901669d0dbfe8975a0ab8204815427eb845391a
victoriamonteiro <victoria.vrsm@gmail.com>
2026-09-06 04:31:37 -0300

    Exercício com o BREAK para parar um LOOP

--- src/EstruturasDeRepeticao03.java
+++ src/EstruturasDeRepeticao03.java
@@ -3,12 +3,26 @@ public class EstruturasDeRepeticao03 {
         // Imprima os primeiros 25 numeros de um dado valor
         // Por exemplo, 15
 
-        int valorMax = 25;
+       /* int valorMax = 25;
         for (int i = 0; i <= valorMax; i++) {
             if (i > 15){
                 break;
             }
             System.out.println(i);
+        } */
+
+        // Exercicio BREAK
+        // Dado um valor de um carro, descubra quantas x ele pode ser parcelado
+        // Condiçao valorParcela >= 1.000,00
+
+        double valorCarro = 5000;
+
+        for (double parcela = 1; parcela <= valorCarro; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
         }
     }
 }
