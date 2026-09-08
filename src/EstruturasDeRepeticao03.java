94b74663cddab56a09223043043735495534fd22
victoriamonteiro <victoria.vrsm@gmail.com>
2026-09-08 19:41:41 -0300

    Logicas para utilizar o BREAK

--- src/EstruturasDeRepeticao03.java
+++ src/EstruturasDeRepeticao03.java
@@ -15,7 +15,7 @@ public class EstruturasDeRepeticao03 {
         // Dado um valor de um carro, descubra quantas x ele pode ser parcelado
         // Condiçao valorParcela >= 1.000,00
 
-        double valorCarro = 5000;
+        double valorCarro = 7500;
 
         for (double parcela = 1; parcela <= valorCarro; parcela++) {
             double valorParcela = valorCarro / parcela;
@@ -23,6 +23,32 @@ public class EstruturasDeRepeticao03 {
                 break;
             }
             System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+
+        // Exercicios do BREAK
+
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+
+        /* Nesse caso tem uma pegadinha
+         java executa o código de cima para baixo
+         linha por linha, respeitando condições e laços */
+
+        /* println acontece antes do if, ou seja, ele imprime o 5
+        No código anterior, o if vinha antes do println
+        por isso o 5 não chegava a ser impresso */
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+
+            if (i == 5) {
+                break;
+            }
         }
     }
 }
